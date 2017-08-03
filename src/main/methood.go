308d33b2c26f7b953e571048e4b22dd05ab3f7d8
suredandan <xrzp@qq.com>
2017-08-03 16:47:45 +0800

    函数示例

---
+++ src/main/methood.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//	methodA(1, 2, 3, 4)
+
+	//	methodB()
+
+	//闭包的用法
+	f := closure(10)
+	fmt.Println(f(1))
+	fmt.Println(f(2))
+}
+
+//这个a是一个slice，类似于python的元组
+func methodA(a ...int) {
+	fmt.Println(a)
+}
+
+func methodB() {
+	a := func() {
+		fmt.Println("这个是匿名函数")
+	}
+	a()
+}
+
+//闭包的用法
+func closure(x int) func(int) int {
+	return func(y int) int {
+		return x + y
+	}
+}
